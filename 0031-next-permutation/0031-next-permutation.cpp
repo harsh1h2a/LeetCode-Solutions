6d3da315f7b324ef979b660b503c954077fa610d
harsh1h2a <harsh1h2a@gmail.com>
2026-07-15 18:08:39 +0530

    Time: 0 ms (100%), Space: 15.8 MB (8.15%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int pivot =-1,n=a.size();
+        for(int i=n-2;i>=0;i--){
+            if(a[i]<a[i+1]){
+                pivot =i;
+                break;
+            }
+        }
+        if(pivot==-1){
+            reverse(a.begin(),a.end());
+            return;
+        }
+        for(int i=n-1;i>pivot;i--){
+            if(a[i]>a[pivot]){
+                swap(a[i],a[pivot]);
+                break;
+            }
+        }
+        int i=pivot+1,j=n-1;
+        while(i<=j){
+            swap(a[i++],a[j--]);
+        }
+              
+
+
+
+    }
+};
