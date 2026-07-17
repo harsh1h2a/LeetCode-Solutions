057d2583a84e81842d3500db2dce38046d3259a7
harsh1h2a <harsh1h2a@gmail.com>
2026-07-17 16:10:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.89%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int re=0;
+        while(n!=0){
+            int dig=n%10;
+            if(re>INT_MAX/10 || re<INT_MIN/10){
+                return 0;
+            }
+            re= re*10+dig;
+            n=n/10;
+        }
+        return re;
+    }
+};
