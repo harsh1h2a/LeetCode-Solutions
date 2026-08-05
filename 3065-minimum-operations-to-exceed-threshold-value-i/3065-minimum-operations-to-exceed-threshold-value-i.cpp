f67589cb5666cb6475d6fc458883e778d54f17b5
harsh1h2a <harsh1h2a@gmail.com>
2026-08-05 22:23:38 +0530

    Time: 0 ms (100%), Space: 25.1 MB (7.84%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i;
+        for(i=0;i<nums.size();i++){
+            if(nums[i]>=k)break;
+        }
+        return i;
+    }
+};
